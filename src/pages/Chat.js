05fb29f19936d8395e964903bae016d2abbff505
Enzo Orbe <enzoforbe@gmail.com>
2021-05-24 11:33:12 -0300

    Maquetacion Chat

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -1,9 +1,19 @@
 import React from "react";
+import { ChatMessages } from "../components/ChatMessages";
+import { ChatSelect } from "../components/ChatSelect";
+import { Users } from "../components/Users";
+import "../css/chat.css";
 
 export const Chat = () => {
   return (
-    <div>
-      <h1>Chat</h1>
+    <div className="messaging">
+      <div className="inbox_msg">
+        <Users />
+        {/* <!-- Chat inicio --> */}
+        {/* <ChatMessages /> */}
+        {/* <!-- Chat Fin --> */}
+        <ChatSelect />
+      </div>
     </div>
   );
 };
